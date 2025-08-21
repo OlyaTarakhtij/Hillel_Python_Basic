e5f73cb1f78acaacf7f94a40d90a0682c1657e1c
Olya Tarakhtij <phd.olya@gmail.com>
2025-08-21 17:35:21 +0300

    Initial commit

---
+++ Hillel_Python_Basic/Home_work_Lesson_5/home_work_5.3.py
@@ -0,0 +1,11 @@
+import string
+
+any_string = input('Введіть якусь строку, будь ласка: ')
+table = str.maketrans('', '', string.punctuation)  # видалення знаків пунктуації
+clean_string = any_string.translate(table)
+hashtag_string = ['#' + ''.join(word.capitalize() for word in clean_string.split())] # переведення першої літери кожного слова на велику
+
+if len(hashtag_string) > 140: # Перевірка довжини строки
+    hashtag_string = hashtag_string[:140] # скорочення до 140 символів
+
+print(hashtag_string)
